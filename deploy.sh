d7ab7f7a498b8b0fbeedfaae9d79f81d3651e06c
Christoph Pader <chrispad2k@gmail.com>
2020-03-21 16:56:01 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,17 @@
-cd build
+#!/bin/bash
+
+LAST_COMMIT=`git log -1 --pretty=%B`
+PUBLISH_REPO="chrispad2k.github.io/"
+
+rm $PUBLISH_REPO/*
+mv -f build/* $PUBLISH_REPO
+rm -rf build/
+
+cd $PUBLISH_REPO
 git add .
-git commit -m "$(git log -1 --pretty=%B)"
+git commit -m "$LAST_COMMIT"
 git push
+
+cd ..
+git add $PUBLISH_REPO
+git commit -m "Deployed to Github Pages"
