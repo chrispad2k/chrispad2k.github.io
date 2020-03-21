ecfd967026b86debcaa1ccd7f60116152da24a3e
Christoph Pader <chrispad2k@gmail.com>
2020-03-21 14:09:28 +0100

    Initial commit: created react app with typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='test'>
+      <div className='top' />
+      <div className='left' />
+      <div className='center' />
+      <div className='right' />
+      <div className='bottom' />
+    </div>
+  );
+}
+
+export default App;
