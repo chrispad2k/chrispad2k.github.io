c23dcbae796070a1a402126ae330008c591eeba0
Christoph Pader <chrispad2k@gmail.com>
2020-03-21 20:58:24 +0100

    updated layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import './App.css';
+import { LoremIpsum } from 'lorem-ipsum';
+import './App.less';
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
 
 function App() {
   return (
-    <div className='test'>
-      <div className='top' />
-      <div className='left' />
-      <div className='center' />
-      <div className='right' />
-      <div className='bottom' />
+    <div className='app'>
+      <div className='header area' id='#header' />
+      <div className='sidebar area' id='#sidebar' />
+      <div className='main area'>{ lorem.generateParagraphs(20) }</div>
+      <div className='footer area' />
     </div>
   );
 }
