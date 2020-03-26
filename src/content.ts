ffb489f83435354a9a1cdd7ea11fd99e9286d803
Christoph Pader <chrispad2k@gmail.com>
2020-03-26 20:00:53 +0100

    added content.ts for strings

---
+++ src/content.ts
@@ -0,0 +1,32 @@
+import { LoremIpsum } from 'lorem-ipsum'
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+})
+
+export default {
+  header: {
+    logo: 'Christoph Pader',
+    intro: {
+      header: 'I\'m a enthusiastic Software Architect and Web Designer',
+      text: lorem.generateSentences(2)
+    }
+  },
+  main: {
+    about: {
+      text: 'I was born in Vienna and i am actually pretty cool!'
+    }
+  },
+  links: {
+    github: 'https://github.com/chrispad2k',
+    linkedin: 'https://www.linkedin.com/in/christoph-pader-6ab7ba142',
+    twitter: 'https://twitter.com/chrispad2k'
+  },
+}
