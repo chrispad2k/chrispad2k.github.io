d972554ff580cdcae9a1e858695c928d963943b4
Christoph Pader <chrispad2k@gmail.com>
2020-03-26 13:06:40 +0100

    added footer and fixed responsiveness for over 1200px

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { LoremIpsum } from 'lorem-ipsum'
 import './App.less'
+import links from '../links.json'
 import circleLeft from '../../public/images/circle-left.svg'
 import circleRight from '../../public/images/circle-right.svg'
 
@@ -20,9 +21,10 @@ class App extends React.Component {
 
   constructor(props) {
     super(props)
-    // this.updateHeader = this.updateHeader.bind(this)
-    // this.headerTextRef = React.createRef()
-    // this.state = { headerTextStyle: {} }
+    this.state = {}
+    // this.updateMain = this.updateMain.bind(this)
+    // this.projectContentBgTop = React.createRef()
+    // this.projectContentBgBottom = React.createRef()
   }
 
   render() {
@@ -42,7 +44,7 @@ class App extends React.Component {
             </ul>
           </nav>
 
-          <div className='text' /* ref={this.headerTextRef} */ /* style={this.state.headerTextStyle} */>
+          <div className='text'>
             <h2>Software Developer</h2>
             <p>{lorem.generateSentences(3)}</p>
           </div>
@@ -57,7 +59,7 @@ class App extends React.Component {
           </section>
 
           <section id='projects'>
-            <svg xmlns='http://www.w3.org/2000/svg' className='bg-top' viewBox='0 0 1440 68' enableBackground='new 0 0 1440 68'>
+            <svg xmlns='http://www.w3.org/2000/svg'className='bg-top' viewBox='0 0 1440 68' enableBackground='new 0 0 1440 68'>
               <path d='m1622.3 1937.7c0 0-410.7 169.1-913.4 75.5-502.7-93.6-977.7 56.3-977.7 56.3v440h1891.1v-571.8' fill='#000' transform='translate(0-1977)'></path>
             </svg>
 
@@ -79,23 +81,58 @@ class App extends React.Component {
           </section>
         </section>
         <section className='footer' id='footer'>
-          <p>Footer</p>
+          <svg xmlns='http://www.w3.org/2000/svg'className='bg-top' viewBox='0 0 1440 68' enableBackground='new 0 0 1440 68'>
+            <path d='m1622.3 1937.7c0 0-410.7 169.1-913.4 75.5-502.7-93.6-977.7 56.3-977.7 56.3v440h1891.1v-571.8' fill='#000' transform='translate(0-1977)'></path>
+          </svg>
+          <div className='content'>
+            <div className='left'>
+              <p>{lorem.generateSentences(2)}</p>
+              <ul>
+                <li><a href={links.github}>GitHub</a></li>
+                <li><a href={links.linkedin}>LinkedIn</a></li>
+                <li><a href={links.twitter}>Twitter</a></li>
+              </ul>
+            </div>
+            <div className='center'>
+              <p>{lorem.generateSentences(2)}</p>
+              <ul>
+                <li><a href='#about'>Impressum</a></li>
+                <li><a href='#projects'>Contact</a></li>
+              </ul>
+            </div>
+            <div className='right'>
+              <p>{lorem.generateSentences(2)}</p>
+              <ul>
+                <li><a href='#'>1</a></li>
+                <li><a href='#'>2</a></li>
+                <li><a href='#'>3</a></li>
+              </ul>
+            </div>
+          </div>
         </section>
       </div>
     )
   }
 
   // componentDidMount() {
-  //   window.addEventListener('resize', this.updateHeader)
-  //   this.updateHeader()
+  //   window.addEventListener('resize', this.updateMain)
+  //   this.updateMain()
   // }
 
   // componentWillUnmount() {
-  //   window.removeEventListener('resize', this.updateHeader)
+  //   window.removeEventListener('resize', this.updateMain)
   // }
 
-  // updateHeader() {
-  //   this.setState({ headerTextStyle: { top: `calc(100vw * (886/1920) - ${this.headerTextRef.current.offsetHeight}px)` } })
+  // updateMain() {
+  //   const bgTopHeight = this.projectContentBgTop.current.getBoundingClientRect().height
+  //   const bgBottomHeight = this.projectContentBgBottom.current.getBoundingClientRect().height
+  //   console.log(bgBottomHeight)
+  //   this.setState({
+  //     projectContentStyle: {
+  //       marginTop: bgTopHeight * -1 + 'px',
+  //       marginBottom: bgBottomHeight * -1 + 'px',
+  //     }
+  //   })
   // }
 }
 
